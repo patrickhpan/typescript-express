2227ebffee45e5f6a9fc8069778da9c37209346f
Patrick Pan <patrickhpan@users.noreply.github.com>
2020-04-26 21:56:32 -0400

    add body-parser

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import loadRoutes from './routes';
 import { getDB } from './db';
 
 (async () => {
   const app = express();
   await getDB();
+  
+  app.use(bodyParser.json());
 
   loadRoutes(app)
 
